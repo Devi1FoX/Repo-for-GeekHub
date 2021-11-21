218649daa1af9280446a297813f5fa81b19b8cb0
Devi1FoX <72566986+Devi1FoX@users.noreply.github.com>
2021-11-21 22:41:24 +0200

    Delete write_a_script_that_concatenates_all_the_items_in_the_list_and_displays_them_on_the_screen_the_list_can_be_hardcode.py

--- write_a_script_that_concatenates_all_the_items_in_the_list_and_displays_them_on_the_screen_the_list_can_be_hardcode.py
+++
@@ -1,8 +0,0 @@
-chek_list = ['морква', 'картошка', 3, 1, 6]
-def concatenate_list_data(list):
-    result = ''
-    for element in list:
-        result += str(element)
-    return result
-
-print(concatenate_list_data(['морква', 'картошка', 3, 1, 6]))
