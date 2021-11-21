f40e654d4fd8e730c1034d31d9357cfda02481fd
Devi1FoX <72566986+Devi1FoX@users.noreply.github.com>
2021-11-21 22:49:54 +0200

    Delete write_a_script_to_concatenate_n_strings.py

--- write_a_script_to_concatenate_n_strings.py
+++
@@ -1,8 +0,0 @@
-some_number = int(input('Write your number: '))
-result = ""
-
-for a in range(some_number):
-    print(a)
-    user_text = input('Write your text:')
-    result = result + user_text
-print(result)
